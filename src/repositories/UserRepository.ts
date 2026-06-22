17388827de89c20274eeaeb10c143459933ee6d0
MatheusBrayer <137443185+MatheusBrayer@users.noreply.github.com>
2026-06-22 16:35:33 -0300

    upgrade UserRepository and index

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -1,7 +1,7 @@
 import { pool } from "../database/connection";
 
 export class UserRepository {
-    async createUser (name: string, email: string, password: string) {
+    async create (name: string, email: string, password: string) {
         const result = await pool.query(
             "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id",
             [name, email, password]
@@ -9,13 +9,27 @@ export class UserRepository {
         return result.rows[0];
     }
 
-    async findAllUsers () {
+    async findAll () {
         const result = await pool.query("SELECT * FROM users");
         return result.rows;
     }
 
-    async findByID (id: number) {
+    async findById (id: number) {
         const result = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
         return result.rows[0];
     }
+
+    async update (id: number, name: string, email: string, password: string) {
+        const rusult = await pool.query(
+            "UPDATE users SET name = $1, email = $2, password = $3 WHERE id = $4 RETURNING *",
+            [name, email, password, id]
+        );
+        return rusult.rows[0];
+    }
+
+    async delete (id: number) {
+        const result = await pool.query("DELETE FROM users WHERE id = $1 RETURNING *", [id]);
+        return result.rows[0];
+    }
+
 }
