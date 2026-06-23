9e7e53b35c1d9d75e6568ffb1a993e2e3b2b56bc
MatheusBrayer <137443185+MatheusBrayer@users.noreply.github.com>
2026-06-23 09:12:02 -0300

    switching to cloud banking

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -3,10 +3,19 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-});
+export const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+      }
+);
